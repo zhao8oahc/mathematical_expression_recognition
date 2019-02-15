a1dcab58539b2271ee29a501f79953ce6b3fcc3e
zhao8oahc <zhao3oahc@gmail.com>
2019-02-15 15:02:33 +0800

    changes in mathematical_expression_recognition.ipynb

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 #-*- encoding: utf-8 -*-
 
 import os
-import cv2
+#import cv2
 import pandas as pd
 import numpy as np
 import random
@@ -10,14 +10,19 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
-from captcha.image import ImageCaptcha
-from PIL import Image
+#from captcha.image import ImageCaptcha
+#from PIL import Image
 
 
 csv_path = 'data/train.csv'
 df_label = pd.read_csv(csv_path, index_col=0)
 
-symbols = string.digits + '+-*=()'
+characters = string.digits + '+-*=()'
 
-im = array(Image.open('').convert('L'),'f')
+#im = array(Image.open('').convert('L'),'f')
 
+#def data(self, dir='Data/train_dir/train', ):
+#	def __init__():
+#
+#
+#	def 
